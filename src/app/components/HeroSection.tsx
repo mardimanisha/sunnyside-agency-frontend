c2f4c1521511f4de2fe48e31524abfcacfad439c
Manisha Mardi <mardimanisha101@gmail.com>
2024-11-28 15:12:03 +0530

    "Updated padding and margin values in HeroSection component and added class to root div in Home component"
    Signed-off-by: Manisha Mardi <mardimanisha101@gmail.com>

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,11 +6,11 @@ export const HeroSection = () => {
   return (
     <div>
       <div className="sm:bg-[url('/images/desktop/image-header.jpg')] bg-[url('/images/mobile/image-header.jpg')] bg-cover bg-center sm:h-screen h-[915px] w-full">
-        <div className='p-2'>
+        <div className='p-4'>
             <Navbar />
-            <div className='flex flex-col justify-evenly items-center my-40 sm:my-0'>
+            <div className='flex flex-col justify-evenly items-center my-40 sm:my-12'>
               <p className="text-white font-fraunces text-center text-5xl sm:text-5xl font-extrabold uppercase tracking-widest m-16 sm:m-10">We are creatives</p>
-              <Image src='/images/icon-arrow-down.svg' alt='arrow_icon' width={40} height={40} className='sm:m-10 m-16 sm:h-26 sm:w-8' />
+              <Image src='/images/icon-arrow-down.svg' alt='arrow_icon' width={40} height={40} className='sm:m-6 m-16 sm:h-26 sm:w-8' />
             </div>
         </div>
       </div>
